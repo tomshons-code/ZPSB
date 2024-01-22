8898a2f5c8ed9a78da47e41598bf95f90f077ab5
Tomasz Przyjemski <73355972+tomshons-code@users.noreply.github.com>
2024-01-22 19:50:33 +0100

    Add files via upload

---
+++ Mod2/Modul02_p1/Problem5/Max.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Max
+{
+    internal class Max
+    {
+        public static void Main(string[] args)
+        {
+            //Deklaracja zmiennych
+            int a, b, max;
+            
+            //Pobranie wartości zmiennych
+            Console.Write("Podaj pierwszą wartość: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj pierwszą wartość: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            //Wyznaczenie warości max
+            max = a > b ? a : b;
+            
+            Console.WriteLine("Wartość maksymalna wynosi: {0}.", max);
+            
+            //Zakończenie programu
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Mod3/Modul03_p1/Problem2/Wyrazenie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Wyrazenie
+{
+    internal class Wyrazenie
+    {
+        public static void Main(string[] args)
+        {
+            //Deklarazja zmiennych
+            double x, y, z;
+            
+            //Pobranie wartośći zmiennych
+            Console.Write("Podaj wartośc zmiennej x:" );
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartośc zmiennej y:" );
+            y = Convert.ToDouble(Console.ReadLine());
+            
+            //Sprawdzenie zmiennych x i y w przypadku mniejszym od zera
+            if (x < 0 && y < 0)
+            {
+                z = x * y; 
+            }
+            else //Sprawdzenie zmiennych x i y w przypadku równym zero
+            {
+                if (x == 0 || y == 0)
+                {
+                    z = 10;
+                }
+                else
+                {
+                    z = x + y;
+                }
+            }
+            
+            //Wypisanie wartości zmiennej
+            Console.Write("z = {0}", z);
+            
+            //Zakończenie programu
+            Console.ReadKey();
+
+        }
+    }
+}
