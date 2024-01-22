2c26c8fd77b400443a09f682d7d3f3e43f4b91bd
Tomasz Przyjemski <73355972+tomshons-code@users.noreply.github.com>
2024-01-22 19:58:25 +0100

    Moduły 2 i 3

---
+++ Część Pierwsza/Mod2/Modul02_p1/Problem1/Zamiana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Modul02
+{
+    internal class Zamiana
+    {
+        public static void Main(string[] args)
+        {
+            //Inicjacja zmiennych
+            double a, b, tmp;
+            
+            //Pobieranie wartości zmiennych a i b od użytkownika
+            Console.WriteLine("Podaj wartość zmiennej a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wartość zmiennej b");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            //wymiana wartości a i b przy pomocy zmiennej tmp
+            tmp = a;
+            a = b;
+            b = tmp;
+            
+            //Wypisanie wartości zmiennych i zakończenie programu
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
