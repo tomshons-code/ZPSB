c8a22edb8d41960638bcf77703dee126ab137635
tomshons-code <tomaszprzyjemski.business@gmail.com>
2024-01-23 18:45:23 +0100

    Problem 1 Mod7

--- Część Pierwsza/Mod7/Modul07-p1/Problem1/Wstawianie.cs
+++ Część Pierwsza/Mod7/Modul07-p1/Problem1/Wstawianie.cs
@@ -19,21 +19,58 @@ namespace Modul07_p1
             {
                 Console.WriteLine("{0:f3}; ", i);   
             }
+            
+            //Sposób 1
+            
+            // double x;
+            // int j;
+            // for (int i = 0; i < tablica.Length; i++)
+            // {
+            //     x = tablica[i];
+            //     for (j = i-1; j >= 0 && x < tablica[j] ; j--)
+            //     {
+            //         tablica[j + 1] = tablica[j];
+            //     }
+            //
+            //     tablica[j + 1] = x; 
+            // }
 
+            //Sposób 2
+            
+            int j, l, p, m;
             double x;
-            int j;
             for (int i = 0; i < tablica.Length; i++)
             {
                 x = tablica[i];
-                for (j = i-1; j >= 0 && x < tablica[j] ; j--)
+                l = 0;
+                p = i - 1;
+                while (l <= p)
+                {
+                    m = (l + p) / 2;
+                    if (x <tablica[m])
+                    {
+                        p = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+            
+                for (j = i-1; j >= l; j--)
                 {
                     tablica[j + 1] = tablica[j];
                 }
-                //tablica[j+1] = //Błąd w treści zadania brakuje informacji o wartości do przypisania
+                tablica[j + 1] = x;
+            }
+            
+            Console.WriteLine("\nElementy tablicy po sortowaniu:");
+            foreach (double i in tablica)
+            {
+                Console.Write("{0:f3}, ", i);
             }
 
-            int l, p, m; //Ponowna deklaracja zmiennej j w teści zadania powoduje konflikt programu
-            //double x;
+            Console.ReadKey();
         }
     }
 }
